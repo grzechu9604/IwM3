52dbc26c64d3f4941d50c14d346fb07c38d8a8eb
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-05-29 14:32:44 +0200

    Możliwość wyboru ilości zwracanych krotek

--- src/main/java/connection/HapiGenericService.java
+++ src/main/java/connection/HapiGenericService.java
@@ -13,18 +13,24 @@ import java.util.List;
 public class HapiGenericService<E extends BaseResource> {
     private HapiService hs;
     private final Class<E> type;
+    private final int maxListSize = 50;
 
     HapiGenericService(HapiService hs, Class<E> type) {
         this.hs = hs;
         this.type = type;
     }
 
-
     public List<E> getList() {
+        return getList(maxListSize);
+    }
+
+
+        public List<E> getList(int maxResultAmount) {
         List<E> list = new ArrayList<>();
         try {
             IBaseBundle bundle = hs.getClient().search().forResource(type)
                     .prettyPrint()
+                    .count(maxResultAmount)
                     .execute();
             ((Bundle) bundle).getEntry().forEach(b -> list.add((E) b.getResource()));
         } catch (Exception e) {
@@ -33,13 +39,18 @@ public class HapiGenericService<E extends BaseResource> {
         return list;
     }
 
-    public List<E> search(ICriterion<StringClientParam> criterion) {
+    public List<E> search(ICriterion<StringClientParam> criterion){
+        return search(criterion, maxListSize);
+    }
+
+    public List<E> search(ICriterion<StringClientParam> criterion, int maxResultsAmount) {
         List<E> list = new ArrayList<>();
         try {
             IBaseBundle bundle = hs.getClient()
                     .search()
                     .forResource(type)
                     .where(criterion)
+                    .count(maxResultsAmount)
                     .execute();
             ((Bundle) bundle).getEntry().forEach(b -> list.add((E) b.getResource()));
         } catch (Exception e) {
